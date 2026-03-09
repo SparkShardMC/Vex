97bf410275f6d37fb829ad6feabeacd09131d203
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-09 14:23:49 -0700

    Update main_menu.py

--- ui/main_menu.py
+++ ui/main_menu.py
@@ -1,98 +1,179 @@
 from direct.showbase.ShowBase import ShowBase
-from direct.gui.DirectGui import DirectButton
-from panda3d.core import TextNode, TransparencyAttrib, Point3, NodePath
-from panda3d.core import Filename, LPoint3, LVector3
-from panda3d.physics import ForceNode, LinearVectorForce
+from panda3d.core import *
+from direct.gui.DirectGui import DirectButton, OnscreenText
+from direct.task import Task
 from direct.particles.ParticleEffect import ParticleEffect
-import random
-import os
+import math, random
 
-class MainMenu(ShowBase):
-
+class VexMainMenu(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         self.disableMouse()
 
-        # ---- Background ----
-        from panda3d.core import CardMaker
+        # Camera
+        self.camera.setPos(0, -16, 4)
+        self.camera.lookAt(0, 0, 1.5)
+        self.time = 0
+
+        # Setup scene
+        self.setup_lighting()
+        self.create_fog()
+        self.create_background()
+        self.create_logo()
+        self.create_soldier()
+        self.create_buttons()
+        self.create_sparks_particles()
+
+        # Tasks
+        taskMgr.add(self.animate_camera, "animate_camera")
+        taskMgr.add(self.animate_soldier, "animate_soldier")
+        taskMgr.add(self.animate_logo, "animate_logo")
+
+    # ---------- Lighting ----------
+    def setup_lighting(self):
+        ambient = AmbientLight("ambient")
+        ambient.setColor((0.3,0.3,0.3,1))
+        ambient_np = render.attachNewNode(ambient)
+        render.setLight(ambient_np)
+
+        directional = DirectionalLight("sun")
+        directional.setColor((1,1,1,1))
+        sun_np = render.attachNewNode(directional)
+        sun_np.setHpr(-45,-45,0)
+        render.setLight(sun_np)
+
+    # ---------- Fog ----------
+    def create_fog(self):
+        fog = Fog("sceneFog")
+        fog.setColor(0,0,0,1)
+        fog.setExpDensity(0.03)
+        render.setFog(fog)
+
+    # ---------- Background ----------
+    def create_background(self):
         cm = CardMaker("bg")
-        cm.setFrame(-5, 5, -5, 5)
-        self.bg = self.render2d.attachNewNode(cm.generate())
-        self.bg.setColor(0,0,0,1)
-        self.bg.setTransparency(TransparencyAttrib.MAlpha)
+        cm.setFrame(-30,30,-20,20)
+        self.bg = render.attachNewNode(cm.generate())
+        self.bg.setPos(0,40,0)
+        self.bg.setColor(0.02,0.02,0.05,1)
 
-        # ---- Spark Particle System ----
-        self.spark = ParticleEffect()
-        self.spark.loadConfig("assets/particles/spark.ptf")
-        self.spark.start(parent=self.render2d, renderParent=self.render2d)
+    # ---------- Logo ----------
+    def create_logo(self):
+        font = loader.loadFont("assets/fonts/vex_inferno.ttf")
+        self.logo = OnscreenText(text="VEX",
+                                 font=font,
+                                 scale=0.3,
+                                 pos=(0,0.75),
+                                 fg=(1,0.2,0.1,1),
+                                 align=TextNode.ACenter)
 
-        # ---- VEX Logo ----
-        self.create_vex_logo()
+    # ---------- Soldier Logo ----------
+    def create_soldier(self):
+        self.soldier = render.attachNewNode("soldier")
 
-        # ---- Main Buttons ----
-        self.play_button = DirectButton(
-            text="PLAY",
-            scale=0.08,
-            pos=(0,0,-0.4),
-            frameColor=(0.2,0.2,0.2,1)
-        )
-        self.settings_button = DirectButton(
-            text="SETTINGS",
-            scale=0.06,
-            pos=(0,0,-0.55),
-            frameColor=(0.2,0.2,0.2,1)
-        )
+        # Body
+        body = loader.loadModel("models/misc/rgbCube")
+        body.reparentTo(self.soldier)
+        body.setScale(0.7,0.4,1.2)
+        body.setPos(0,0,1.2)
 
-        # ---- Logo Text Pulse Task ----
-        taskMgr.add(self.pulse_text, "PulseTextTask")
-        self.pulse_scale = 0.2
-        self.pulse_direction = 1
+        # Head
+        head = loader.loadModel("models/misc/sphere")
+        head.reparentTo(self.soldier)
+        head.setScale(0.4)
+        head.setPos(0,0,2.3)
 
-    # ---- Logo ----
-    def create_vex_logo(self):
-        # Load soldier model
-        soldier_model = "assets/models/soldier.bam"
-        if os.path.exists(soldier_model):
-            soldier = self.loader.loadModel(soldier_model)
-        else:
-            soldier = self.loader.loadModel("models/misc/rgbCube")
-        soldier.reparentTo(self.render2d)
-        soldier.setScale(0.3)
-        soldier.setPos(-0.1,0,0.1)
-        soldier.setColor(0.8,0.8,0.8,1)
+        # Legs
+        self.left_leg = loader.loadModel("models/misc/rgbCube")
+        self.left_leg.reparentTo(self.soldier)
+        self.left_leg.setScale(0.25,0.25,1)
+        self.left_leg.setPos(-0.25,0,0.5)
 
-        # Load shield model
-        shield_model = "assets/models/shield.bam"
-        if os.path.exists(shield_model):
-            shield = self.loader.loadModel(shield_model)
-        else:
-            shield = self.loader.loadModel("models/misc/sphere")
-        shield.reparentTo(self.render2d)
-        shield.setScale(0.2)
-        shield.setPos(0.2,0,0.1)
-        shield.setColor(0.7,0.7,0.7,1)
+        self.right_leg = loader.loadModel("models/misc/rgbCube")
+        self.right_leg.reparentTo(self.soldier)
+        self.right_leg.setScale(0.25,0.25,1)
+        self.right_leg.setPos(0.25,0,0.5)
 
-        # VEX text
-        font_path = "assets/fonts/vex_inferno.ttf"
-        if os.path.exists(font_path):
-            vex_font = self.loader.loadFont(font_path)
-        else:
-            vex_font = None
+        # Arms
+        self.left_arm = loader.loadModel("models/misc/rgbCube")
+        self.left_arm.reparentTo(self.soldier)
+        self.left_arm.setScale(0.2,0.2,0.9)
+        self.left_arm.setPos(-0.8,0,1.6)
 
-        vex_text = TextNode("vex_text")
-        vex_text.setText("VEX")
-        if vex_font:
-            vex_text.setFont(vex_font)
-        vex_text.setAlign(TextNode.ACenter)
-        self.vex_node = self.aspect2d.attachNewNode(vex_text)
-        self.vex_node.setScale(self.pulse_scale)
-        self.vex_node.setPos(0,0,0.5)
-        self.vex_node.setColor(1,0.2,0.2,1)
+        shield = loader.loadModel("models/misc/sphere")
+        shield.reparentTo(self.left_arm)
+        shield.setScale(0.6,0.15,0.6)
+        shield.setPos(-0.5,0,0)
+        shield.setColor(0.1,0.8,1,1)  # glowing shield
 
-    # ---- Pulse Effect for Text ----
-    def pulse_text(self, task):
-        self.pulse_scale += 0.001 * self.pulse_direction
-        if self.pulse_scale > 0.22 or self.pulse_scale < 0.18:
-            self.pulse_direction *= -1
-        self.vex_node.setScale(self.pulse_scale)
-        return task.cont
+        self.right_arm = loader.loadModel("models/misc/rgbCube")
+        self.right_arm.reparentTo(self.soldier)
+        self.right_arm.setScale(0.2,0.2,0.9)
+        self.right_arm.setPos(0.8,0,1.6)
+
+        sword = loader.loadModel("models/misc/rgbCube")
+        sword.reparentTo(self.right_arm)
+        sword.setScale(0.1,0.1,1.4)
+        sword.setPos(0,0,1)
+        sword.setColor(1,0.6,0.1,1)  # glowing sword
+
+    # ---------- Buttons ----------
+    def create_buttons(self):
+        self.play_button = DirectButton(text="PLAY", scale=0.08, pos=(0,0,-0.35),
+                                        frameColor=(0.15,0.15,0.15,1),
+                                        command=self.play_game)
+        self.settings_button = DirectButton(text="SETTINGS", scale=0.06, pos=(0,0,-0.5),
+                                            frameColor=(0.15,0.15,0.15,1),
+                                            command=self.settings)
+
+    # ---------- Particle Sparks ----------
+    def create_sparks_particles(self):
+        self.sparks = []
+        for i in range(100):
+            spark = loader.loadModel("models/misc/sphere")
+            spark.reparentTo(render)
+            spark.setScale(0.03)
+            spark.setColor(1,random.uniform(0.4,0.8),0.1,1)
+            spark.setPos(random.uniform(-4,4), random.uniform(-3,4), random.uniform(0,4))
+            self.sparks.append(spark)
+
+    # ---------- Animations ----------
+    def animate_camera(self, task):
+        self.time += globalClock.getDt()
+        x = math.sin(self.time*0.2)*3
+        y = -16 + math.cos(self.time*0.2)*2
+        z = 4 + math.sin(self.time*0.1)*0.3
+        self.camera.setPos(x,y,z)
+        self.camera.lookAt(0,0,1.5)
+        return Task.cont
+
+    def animate_soldier(self, task):
+        t = globalClock.getFrameTime()*3
+        leg = math.sin(t)*30
+        arm = math.sin(t)*-30
+        self.left_leg.setP(leg)
+        self.right_leg.setP(-leg)
+        self.left_arm.setP(arm)
+        self.right_arm.setP(-arm)
+
+        # Update sparks
+        for spark in self.sparks:
+            spark.setZ(spark.getZ()-0.03)
+            if spark.getZ()<0: spark.setZ(random.uniform(3,6))
+        return Task.cont
+
+    def animate_logo(self, task):
+        scale = 0.3 + math.sin(globalClock.getFrameTime()*2)*0.03
+        self.logo.setScale(scale)
+        return Task.cont
+
+    # ---------- Button Actions ----------
+    def play_game(self):
+        print("Launching Game...")
+
+    def settings(self):
+        print("Opening Settings...")
+
+
+app = VexMainMenu()
+app.run()
