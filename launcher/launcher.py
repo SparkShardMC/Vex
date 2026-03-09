5b2791cb728ebb9459629ea198dbb60b674c9813
SparkShardMC <zaki.coding0119@gmail.com>
2026-03-09 09:36:48 -0700

    Update launcher.py

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,19 +1,8 @@
-from updater import check_for_updates
-from installer import install_game
-from os_detector import get_os
+from launcher_screen import LauncherScreen
 
 def start_launcher():
-
-    system = get_os()
-    print("Operating System:", system)
-
-    update = check_for_updates()
-
-    if update:
-        print("Update available")
-        install_game(system)
-    else:
-        print("Vex is up to date")
+    screen = LauncherScreen()
+    screen.run()
 
 if __name__ == "__main__":
     start_launcher()
